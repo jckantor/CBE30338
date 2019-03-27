48c4dbd0d549d1af3a2811f74b232fdaec9b9dfb
Jeffrey Kantor <Kantor.1@nd.edu>
2019-03-27 18:44:31 -0400

    add br after colab badge

--- tools/add_navigation.py
+++ tools/add_navigation.py
@@ -22,7 +22,7 @@ NAV_COMMENT = "<!--NAVIGATION-->\n"
 
 COLAB_LINK = """
 <a href="https://colab.research.google.com/github/jckantor/CBE30338/blob/master/notebooks/{notebook_filename}"><img align="left" src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open in Colab" title="Open and Execute in Google Colaboratory"></a>
-
+<br>
 """
 
 
